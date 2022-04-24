f65ee7fcfcd1bf4ec19242146ed42859a189f6c7
unknown <jmlee.study@gmail.com>
2022-04-24 20:37:19 +0900

    bgimg

--- app/src/main/java/com/example/dialist/Store.java
+++ app/src/main/java/com/example/dialist/Store.java
@@ -1,6 +1,9 @@
 package com.example.dialist;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
